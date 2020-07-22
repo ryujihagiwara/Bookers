a5e93ec520ce4da46cf8e1f6815d441dcaa2aa4d
ryujihagiwara <dragon7103next@icloud.com>
2020-07-22 23:58:17 +0900

    [Add] 詳細画面

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,7 @@ class BooksController < ApplicationController
   def create
   	book=Book.new(book_params)
   	book.save
-  	redirect_to '/'
+  	redirect_to book_path(book)
   end
 
   def index
@@ -17,9 +17,17 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book=Book.find(params[:id])
   end
 
   def edit
+    @book=Book.find(params[:id])
+  end
+
+  def update
+    book=Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
   end
   private
 
