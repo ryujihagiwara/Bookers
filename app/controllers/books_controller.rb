2e61871a61dad0e5b93290864ec503e197f1de66
ryujihagiwara <dragon7103next@icloud.com>
2020-07-23 18:53:50 +0900

    [Add] メッセージの表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,9 +6,10 @@ class BooksController < ApplicationController
   	@book=Book.new(book_params)
 
   	if @book.save
+      flash[:notice] = "Book was successfully created"
   	  redirect_to book_path(@book.id)
     else
-      render :index
+      render :new
     end
   end
 
@@ -28,9 +29,12 @@ class BooksController < ApplicationController
   def update
     book=Book.find(params[:id])
 
-    book.update(book_params)
-
-    redirect_to book_path(book)
+    if book.update(book_params)
+      flash[:notice] = "Book was successfully updated"
+       redirect_to book_path(book)
+    else
+      render :edit
+    end
   end
 
   def destroy
