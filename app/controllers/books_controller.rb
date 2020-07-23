e9cded0e05d7a3e23b7d25b8b940f2b10dbe5176
ryujihagiwara <dragon7103next@icloud.com>
2020-07-23 17:17:51 +0900

    [Edit] 一覧表示画面

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,22 +2,19 @@ class BooksController < ApplicationController
   def top
   end
 
-  def new
-  	@book=Book.new
-  end
-
   def create
-  	book=Book.new(book_params)
+  	@book=Book.new(book_params)
 
-  	if book.save
-  	  redirect_to book_path(book.id)
+  	if @book.save
+  	  redirect_to book_path(@book.id)
     else
-      render "new"
+      render :index
     end
   end
 
   def index
   	@books=Book.all
+    @book=Book.new
   end
 
   def show
