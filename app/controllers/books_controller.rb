f2dd76acce83b0aac065520c8eb08133861049a0
ryujihagiwara <dragon7103next@icloud.com>
2020-07-25 19:07:04 +0900

    [Edit] updateアクション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -28,13 +28,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book=Book.find(params[:id])
+    @book=Book.find(params[:id])
 
-    if book.update(book_params)
+    if @book.update(book_params)
       flash[:notice] = "Book was successfully updated"
-       redirect_to book_path(book)
+       redirect_to book_path(@book.id)
     else
-      render :show
+      render :edit
     end
   end
 
