86d103aa996e35d241209f9bf24a3d58c2592b72
ryujihagiwara <dragon7103next@icloud.com>
2020-11-20 22:06:20 +0900

    rubocop導入

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   get 'books/:id' => 'books#show', as: 'book'
 
-  get 'books/:id/edit' => 'books#edit', as:'edit_book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
 
   patch 'books/:id' => 'books#update'
 
